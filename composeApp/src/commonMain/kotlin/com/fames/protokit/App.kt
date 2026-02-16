c29be20e5089e99198ca6a9c182d924be113548b
Migueel Angel Salazar <miguelangel.salazar@smarting.es>
2026-02-16 15:53:01 +0100

    m

--- composeApp/src/commonMain/kotlin/com/fames/protokit/App.kt
+++ composeApp/src/commonMain/kotlin/com/fames/protokit/App.kt
@@ -1,6 +1,10 @@
 package com.fames.protokit
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,12 +13,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fames.protokit.runtime.ProtoKitClient
 import example.ExampleRequest
 import example.ExampleServiceClient
-import grpcbin.DummyRequest
-import grpcbin.GRPCBinClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
@@ -25,33 +30,34 @@ fun App() {
     var state by remember { mutableStateOf<UiState>(UiState.Loading) }
     var strResponse by remember { mutableStateOf("") }
 
-    LaunchedEffect(Unit) { withContext(Dispatchers.IO) {
-        val client = ExampleServiceClient(ProtoKitClient(
-            provideGrpcTransport("https://gref38b0c28f03.free.beeceptor.com")
-        ))
-        val response = client.exampleMethod(request = ExampleRequest("hi"))
-        println("** Response: ${response.message}")
-        state = UiState.Idle
-    } }
-
-    AnimatedContent(state) { s ->
-        when(s) {
-            UiState.Loading -> {
-                CircularProgressIndicator()
-            }
-            is UiState.Error -> {
-                Text((state as UiState.Error).message)
-            }
-            UiState.Idle -> {
-                Text(strResponse)
-            }
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.IO) {
+            val client = ExampleServiceClient(ProtoKitClient(provideGrpcTransport("https://gref38b0c28f03.free.beeceptor.com")))
+            val response = client.exampleMethod(request = ExampleRequest("hi"))
+            println("** Response: ${response.message}")
+            state = UiState.Success
         }
     }
 
+    Column(
+        modifier = Modifier.fillMaxSize().padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        AnimatedContent(state) { s ->
+            when(s) {
+                UiState.Loading -> {
+                    CircularProgressIndicator()
+                }
+                UiState.Success -> {
+                    Text(strResponse)
+                }
+            }
+        }
+    }
 }
 
 sealed class UiState {
     object Loading : UiState()
-    data class Error(val message: String) : UiState()
-    data object Idle : UiState()
+    data object Success : UiState()
 }
