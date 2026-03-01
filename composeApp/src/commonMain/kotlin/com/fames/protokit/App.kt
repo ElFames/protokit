ab2b8be3263be80be50ee3c2cff8a8498b4df879
Migueel Angel Salazar <miguelangel.salazar@smarting.es>
2026-03-01 14:47:56 +0100

    mod ProtoReader.kt

--- composeApp/src/commonMain/kotlin/com/fames/protokit/App.kt
+++ composeApp/src/commonMain/kotlin/com/fames/protokit/App.kt
@@ -1,79 +1,28 @@
 package com.fames.protokit
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.fames.protokit.sdk.ProtoClient
 import com.fames.protokit.sdk.models.Platform
-import com.fames.protokit.sdk.models.onFailure
-import com.fames.protokit.sdk.models.onSuccess
 import com.fames.protokit.sdk.models.platform
 import es.smarting.motorcloud.apis.baseremoteapi.grpc.services.DeviceContextServiceClient
 import es.smarting.motorcloud.apis.baseremoteapi.grpc.services.DeviceInfoProto
 import es.smarting.motorcloud.apis.baseremoteapi.grpc.services.DeviceRequest
 import es.smarting.motorcloud.apis.baseremoteapi.grpc.services.DeviceSessionInfoProto
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
-import kotlinx.coroutines.withContext
+import es.smarting.motorcloud.apis.baseremoteapi.grpc.services.DeviceVerificationRequest
 
 @Composable
 @Preview
 fun App() {
-    val client = ProtoClient("")
-    val service = DeviceContextServiceClient(client)
-
-    var state by remember { mutableStateOf<UiState>(UiState.Loading) }
-    var strResponse by remember { mutableStateOf("") }
-
     LaunchedEffect(Unit) {
-        withContext(Dispatchers.IO) {
-
-            val response = service.openSession(testRequest)
-
-            strResponse = response.toString()
-            println(strResponse)
-            state = UiState.Idle
-        }
-    }
-
-    Column(
-        modifier = Modifier.fillMaxSize().padding(32.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        AnimatedContent(state) { s ->
-            when(s) {
-                UiState.Loading -> {
-                    CircularProgressIndicator()
-                }
-                UiState.Idle -> {
-                    Text(strResponse)
-                }
-            }
-        }
+        val client = ProtoClient("")
+        val service = DeviceContextServiceClient(client)
+        val response = service.openSession(deviceRequestTest)
     }
 }
 
-sealed class UiState {
-    object Loading : UiState()
-    data object Idle : UiState()
-}
-
-val testRequest = DeviceRequest(DeviceInfoProto(
+val deviceInfoProtoTest = DeviceInfoProto(
     deviceId = "gdgd".encodeToByteArray(),
     appInstanceId = "gdgd".encodeToByteArray(),
     operatingSystem = if (platform() == Platform.ANDROID) {
@@ -96,4 +45,6 @@ val testRequest = DeviceRequest(DeviceInfoProto(
     versionCode = 3,
     appVersionName = "gdgd",
     apnsToken = "gdgd".encodeToByteArray()
-))
+)
+val deviceRequestTest = DeviceRequest(deviceInfoProtoTest)
+val deviceVerificationRequestTest = DeviceVerificationRequest(deviceInfoProtoTest, listOf())
